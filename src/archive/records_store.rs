a81ba67e4e8d45d899de905f1642cdfb6fe45831
Davide Ghilardi <dghila@d71.dev>
2023-08-24 11:55:31 +0200

    store exif data as base64

--- src/archive/records_store.rs
+++ src/archive/records_store.rs
@@ -35,7 +35,7 @@ impl PhotoArchiveRecordsStore {
             source: row.source_id,
             path: row.source_path.as_os_str().to_str().map(ToString::to_string).unwrap_or_default(),
             exif: row.exif
-                .map(|exif| exif.fields().map(|f| (format!("{}:{}", f.tag.number(), f.ifd_num.0), f.display_value().to_string())).collect::<HashMap<String, String>>())
+                .map(|exif| Vec::from(exif.buf()))
                 .unwrap_or_default(),
             size: row.size,
             height: row.height,
@@ -63,13 +63,31 @@ struct PhotoArchiveJsonRow {
     source: String,
     #[serde(rename="pth")]
     path: String,
-    #[serde(rename="exf")]
-    exif: HashMap<String, String>,
+    #[serde(rename="exf", with="base64")]
+    exif: Vec<u8>,
     #[serde(rename="siz")]
     size: u64,
     #[serde(rename="hgh")]
     height: u32,
     #[serde(rename="wdt")]
     width: u32,
+}
+
+mod base64 {
+    use serde::{Serialize, Deserialize};
+    use serde::{Deserializer, Serializer};
+    use base64::engine::general_purpose::STANDARD;
+    use base64::Engine;
+
+    pub fn serialize<S: Serializer>(v: &Vec<u8>, s: S) -> Result<S::Ok, S::Error> {
+        let base64 = STANDARD.encode(v);
+        String::serialize(&base64, s)
+    }
+
+    pub fn deserialize<'de, D: Deserializer<'de>>(d: D) -> Result<Vec<u8>, D::Error> {
+        let base64 = String::deserialize(d)?;
+        STANDARD.decode(base64.as_bytes())
+            .map_err(|e| serde::de::Error::custom(e))
+    }
 }
 
